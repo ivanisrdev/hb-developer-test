4bf1ef711cae038863413722d034db048b16d88b
ivanisrdev <ivan.sanchez3@gmail.com>
2023-01-23 07:11:57 +0100

    create spring boot module

---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/HackertestApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbeds.supplierintegrations.hackertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackertestApplication.class, args);
+    }
+
+}
